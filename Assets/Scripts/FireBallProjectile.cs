cee42e5ecc1d8fa6ccd230ece7db4830a95e0a2e
Lyccass <lukasandvoss@gmail.com>
2025-01-13 14:07:34 +0100

    Added a check to not hit the active unit

--- Assets/Scripts/FireBallProjectile.cs
+++ Assets/Scripts/FireBallProjectile.cs
@@ -7,46 +7,51 @@ public class FireBallProjectile : MonoBehaviour
 {
     [SerializeField] float fireballRadius = 4f;
     [SerializeField] private Transform fireballPrefab;
-     [SerializeField] private TrailRenderer trailRenderer;
-    // Start is called before the first frame update
+    [SerializeField] private TrailRenderer trailRenderer;
+
     private Vector3 targetPosition;
     private Action onFireballBehaviourComplete;
+    private Unit activeUnit; // Reference to the unit that activated the ability
 
     private void Update()
     {
         Vector3 moveDir = (targetPosition - transform.position).normalized;
         float moveSpeed = 15f;
         transform.position += moveDir * moveSpeed * Time.deltaTime;
+
         float reachedTargetDistance = .2f;
-        if(Vector3.Distance(transform.position, targetPosition) < reachedTargetDistance)
+        if (Vector3.Distance(transform.position, targetPosition) < reachedTargetDistance)
         {
             Collider[] colliderArray = Physics.OverlapSphere(targetPosition, fireballRadius);
 
-            foreach(Collider collider in colliderArray)
+            foreach (Collider collider in colliderArray)
             {
-                if(collider.TryGetComponent<Unit>(out Unit targetUnit))
+                if (collider.TryGetComponent<Unit>(out Unit targetUnit))
                 {
-                    targetUnit.Damage( UnityEngine.Random.Range(1, 3));
+                    // Exclude the active unit from taking damage
+                    if (targetUnit != activeUnit)
+                    {
+                        targetUnit.Damage(UnityEngine.Random.Range(1, 3));
+                    }
                 }
 
-                if(collider.TryGetComponent<DestructableCrate>(out DestructableCrate destructableCrate))
+                if (collider.TryGetComponent<DestructableCrate>(out DestructableCrate destructableCrate))
                 {
                     destructableCrate.Damage();
                 }
             }
 
-
             trailRenderer.transform.parent = null;
-            Instantiate(fireballPrefab, targetPosition + Vector3.up*1.2f , Quaternion.identity);
+            Instantiate(fireballPrefab, targetPosition + Vector3.up * 1.2f, Quaternion.identity);
             Destroy(gameObject);
-            onFireballBehaviourComplete();
+            onFireballBehaviourComplete?.Invoke();
         }
     }
-    public void Setup(GridPosition targetGridPosition, Action onFireballBehaviourComplete)
+
+    public void Setup(GridPosition targetGridPosition, Action onFireballBehaviourComplete, Unit activeUnit)
     {
         this.onFireballBehaviourComplete = onFireballBehaviourComplete;
+        this.activeUnit = activeUnit; // Store the reference to the active unit
         targetPosition = LevelGrid.Instance.GetWorldPositionn(targetGridPosition);
     }
-
 }
-
